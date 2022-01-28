2d7c06fa224c48b25f3b4e4a6598fb9edf0ef262
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-01-28 17:45:55 +0100

    Add skin command

---
+++ commands/skin.js
@@ -0,0 +1,94 @@
+/*
+ * mixelblocks-discordbot | Copyright (C) 2022 | mixelblocks.de
+ * Licensed under the MIT License
+ *
+ *
+ *              $$\                  $$\\$\      $$\                  $$\                       $$\
+ *              \__|                 $$ $$ |     $$ |                 $$ |                      $$ |
+ * $$$$$$\\$$$\ $$\\$\   $$\ $$$$$$\ $$ $$$$$$$\ $$ |$$$$$$\  $$$$$$$\$ |  $$\ $$$$$$$\   $$$$$$$ |$$$$$$\
+ * $$  _$$  _$$\\$ \\$\ $$  $$  __$$\\$ $$  __$$\\$ $$  __$$\\$  _____$$ | $$  $$  _____| $$  __$$ $$  __$$\
+ * $$ / $$ / $$ $$ |\\$$$  /$$$$$$$$ $$ $$ |  $$ $$ $$ /  $$ $$ /     $$$$$$  /\\$$$$$\   $$ /  $$ $$$$$$$$ |
+ * $$ | $$ | $$ $$ |$$  $$< $$   ____$$ $$ |  $$ $$ $$ |  $$ $$ |     $$  _$$<  \____$$\  $$ |  $$ $$   ____|
+ * $$ | $$ | $$ $$ $$  /\\$\\\$$$$$$\\$ $$$$$$$  $$ \\$$$$$  \\$$$$$$\\$ | \\$\\$$$$$$  $$\\$$$$$$ \\$$$$$$\
+ * \__| \__| \__\__\__/  \__|\_______\__\_______/\__|\______/ \_______\__|  \__\_______/\__\_______|\_______|
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * You should have received a copy of the MIT License
+ *
+ * Repository:
+ *     Github:          https://github.com/MixelBlccks/mixelblocks-discordbot
+ *
+ * Contact:
+ *     Discord Server:  https://mixelblocks.de/discord
+ *     Website:         https://mixelblocks.de/
+ *     DashBoard:       https://dash.mixelblocks.de/
+ *     Mail:            contact@mixelblocks.de
+ *     Minecraft:       mixelblocks.de:25565
+ *
+ * @author LuciferMorningstarDev - https://github.com/LuciferMorningstarDev
+ * @since 28.01.2022
+ *
+ */
+
+module.exports.run = async (bot, message, label, args, prefix) => {
+    return new Promise(async (resolve, reject) => {
+        const Discord = moduleRequire('discord.js');
+        try {
+            if (!args[0]) {
+                bot.usage(
+                    message,
+                    'Fehler bei der Benutzung des Commands!',
+                    `
+Benutzung: ${prefix + label} <name | uuid >             
+            `
+                );
+                return resolve(false);
+            }
+
+            bot.tools.discord.sendEmbed(message.channel, {
+                title: `Hier ist der Skin von: **${args[0]}**`,
+                thumbnail: `https://mc-heads.net/avatar/${args[0]}`,
+                image: `https://mc-heads.net/body/${args[0]}/right`,
+                description: `
+[Download](https://mc-heads.net/download/${args[0]})
+[Ansehen](https://mc-heads.net/skin/${args[0]})      
+                `,
+            });
+        } catch (error) {
+            bot.error('Error in skin Command.', error);
+            try {
+                let errorEmbed = new Discord.MessageEmbed();
+                errorEmbed.setTitle('Error!');
+                errorEmbed.setDescription('```' + error.stack.slice(0, 1990) + '```');
+                errorEmbed.setColor('#ff0000');
+                message.channel.send({ embeds: [errorEmbed] });
+            } catch (err) {
+                bot.logger.logError(err);
+            }
+            return resolve(false);
+        }
+        return resolve(true);
+    });
+};
+
+module.exports.active = true;
+module.exports.similarityCheck = false;
+
+module.exports.aliases = [];
+
+module.exports.help = {
+    name: 'command_template',
+    category: 'tools',
+    aliases: this.aliases,
+    active: this.active,
+    description: 'Dies ist ein toller Command für dich.',
+    usage: '{prefix}{name} <args>',
+};
