fbd1963e84e7075034300c4fdcd44b0135d0ac3b
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-02-03 16:49:48 +0100

    add ticket creation

--- interactions/create_new_ticket.js
+++ interactions/create_new_ticket.js
@@ -38,6 +38,8 @@
  *
  */
 
+const { Snowflake } = moduleRequire('nodejs-snowflake');
+
 // TODO: add ids to configs
 const openedID = '938800588077015071'; // ID of category for opened tickets
 const closedID = '938800632570187796'; // ID of category for closed tickets
@@ -48,8 +50,65 @@ module.exports.run = async (bot, interaction) => {
     try {
         if (interaction.member.id != '427212136134213644') return interaction.reply({ content: 'Dieses Feature ist noch nicht implementiert.', ephemeral: true });
 
-        interaction.reply({ content: 'Channel Created.', ephemeral: true });
+        var categoryOpened = await bot.channels.fetch(openedID);
+        var categoryClosed = await bot.channels.fetch(closedID);
+
+        if (!categoryOpened || !categoryClosed) return interaction.reply({ content: 'Die Einrichtung des Ticketsystems ist nicht abgeschlossen.', ephemeral: true });
+
+        if (categoryOpened.children.size >= 50) return interaction.reply({ content: 'Aktuell sind zuviele Tickets geöffnet. Bitte versuche es später erneut.', ephemeral: true });
+
+        const uid = new Snowflake();
+        var newTicketID = uid.getUniqueID();
+
+        var newTicketChannel = await interaction.guild.channels.create('open-' + newTicketID, {
+            reason: 'Ticket opened',
+            type: 'GUILD_TEXT',
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.id,
+                    deny: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                },
+                {
+                    id: interaction.member.id,
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                },
+                {
+                    id: '938822964634325002', // TODO: config ticket supporter role entry
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                },
+            ],
+        });
+
+        await newTicketChannel.setParent(openedID);
+
+        var dbEntryDate = Date.now();
+        await bot.db.insertAsync('tickets', {
+            id: '' + newTicketID,
+            creator: interaction.member.id,
+            channel: newTicketChannel.id,
+            createdAt: dbEntryDate,
+            messages: [{ at: dbEntryDate, user: 'system', content: 'Created...' }],
+        });
+
+        var embed = await bot.tools.discord.generateEmbed({
+            title: 'Hier ist dein neues Ticket!',
+            color: 'RED',
+            description: `
+Bitte beschreibe uns ausführlich dein Anliegen und bitte uns stelle keine MetaFragen. 
+
+[Was ist eine MetaFrage?](http://metafrage.de/)
+            `,
+        });
+
+        await newTicketChannel.send({
+            content: '<@' + interaction.member.id + '>',
+            embeds: [embed],
+            components: [new Discord.MessageActionRow().addComponents([new Discord.MessageButton().setLabel('Ticket schliessen').setStyle('DANGER').setEmoji('🎫').setCustomId('close_ticket'), new Discord.MessageButton().setLabel('Ticket löschen').setStyle('DANGER').setEmoji('🎫').setCustomId('delete_ticket')])],
+        });
+
+        interaction.reply({ content: 'Dein Ticket wurde erstellt. <#' + newTicketChannel.id + '>', ephemeral: true });
     } catch (error) {
+        console.error(error);
         bot.error('Error in Interaction Command create_new_ticket', error);
     }
 };
