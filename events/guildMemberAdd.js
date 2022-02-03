2acd1ad4473ab7ae9d86cd971f7286c6265fe200
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-02-03 15:16:27 +0100

    Await embed generation in member join guild event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -49,25 +49,29 @@ module.exports = async (bot, member) => {
         bot.catch(error);
     }
 
-    var index = Math.floor(Math.random() * bot.configs.welcome.messages.length);
-    if (index < 0) index = 0;
-    if (index >= bot.configs.welcome.messages.length) index = bot.configs.welcome.messages.length - 1;
+    try {
+        var index = Math.floor(Math.random() * bot.configs.welcome.messages.length);
+        if (index < 0) index = 0;
+        if (index >= bot.configs.welcome.messages.length) index = bot.configs.welcome.messages.length - 1;
 
-    let message = bot.configs.welcome.messages[index]
-        .replace(/{user}/g, `<@${member.id}>`)
-        .replace(/{username}/g, member.user.username)
-        .replace(/{member_count}/g, member.guild.memberCount)
-        .replace(/{server_name}/g, member.guild.name)
-        .replace(/{rules_channel}/g, `<#914812681754652672>`);
+        let message = bot.configs.welcome.messages[index]
+            .replace(/{user}/g, `<@${member.id}>`)
+            .replace(/{username}/g, member.user.username)
+            .replace(/{member_count}/g, member.guild.memberCount)
+            .replace(/{server_name}/g, member.guild.name)
+            .replace(/{rules_channel}/g, `<#914812681754652672>`);
 
-    var embed = bot.tools.discord.generateEmbed({
-        title: 'Jemand neues ist angekommen :)',
-        description: message,
-        color: '#00EE00',
-    });
-    member.guild.channels.fetch(bot.configs.welcome.channel).then((channel) => {
-        channel.send('<@' + member.id + '>', { embeds: [embed] });
-    });
+        var embed = await bot.tools.discord.generateEmbed({
+            title: 'Jemand neues ist angekommen :)',
+            description: message,
+            color: '#00EE00',
+        });
+        member.guild.channels.fetch(bot.configs.welcome.channel).then((channel) => {
+            channel.send('<@' + member.id + '>', { embeds: [embed] });
+        });
+    } catch (err) {
+        bot.error(err);
+    }
 
     var trustScore = await bot.tools.discord.calculateTrustScore(bot, member);
     var trustScoreAsNumber = 0 + trustScore[0];
